2b566c4d943836707dbe997d4fb9e933add1d193
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-03-03 17:49:12 +0100

    Renamed randomcategorizer.py to random_categorizer.py

---
+++ categorizers/random_categorizer.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+
+import time
+import pickle
+import redis
+import random
+
+from .celery import celeryapp
+from .base import Categorizer
+
+
+class RandomCategorizer(Categorizer):
+    """
+    Just a stupid example on how a categorizer should work.
+    This very serious and useful categorizer just pops a point from the source
+    (no need for another buffer, since it elaborates one point at a time) and
+    waits for a second.
+    """
+
+    @classmethod
+    @celeryapp.task
+    def _run(cls, auth_id):
+        redis_client = redis.StrictRedis()
+
+        with open('output.txt', 'wb') as f:
+            while True:
+                raw_data = redis_client.lpop(cls.gen_key(auth_id))
+                if raw_data is None:
+                    break
+                data = pickle.loads(raw_data)
+
+                # DO SOMETHING WITH THE DATA
+                time.sleep(random.random())
+
+                f.write('{0} :: {1}\n'.format(data['ts'], data['type']))
+
+        cls.unlock.delay(cls)
+
+
+
