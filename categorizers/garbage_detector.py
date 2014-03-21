476ccf674e523cd659034a14818d4ebe8a8753b0
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-03-21 10:54:52 +0100

    First (very simple) working garbage detector.

--- categorizers/garbage_detector.py
+++ categorizers/garbage_detector.py
@@ -3,36 +3,39 @@ from __future__ import absolute_import
 import pickle
 
 from .celery import celeryapp
-from .base import Categorizer, redis_client
+from .base import Categorizer, redis_client, singleton_task
+
+
+# todo: replace those helper functions with real res backend
+# (this is a temporary solution, since result backend is to be defined yet)
+def set_garbage(point):
+    return redis_client.sadd('garbage_points', point)
+
+
+def is_garbage(point):
+    return redis_client.sismember('garbage_points', point)
 
 
 class GarbagePointCategorizer(Categorizer):
     """
     Detect garbage points (points outside the tracks, or points which don't
-    seem to be believable.
+    seem to be believable).
     """
-
-    BUFFER_LENGTH = 10
+    PRECISION_THRESHOLD = 50  # m
 
     @classmethod
     @celeryapp.task
-    def _run(cls, auth_id):
+    @singleton_task
+    def run(cls, auth_id):
         while True:
             # pop data
             raw_data = redis_client.lpop(cls.gen_key(auth_id))
             if raw_data is None:
                 break
 
-            # insert data in buffer
-            buf_key = cls.gen_key(auth_id, 'buf')
-            redis_client.rpush(buf_key, raw_data)
-            if redis_client.llen(buf_key) >= cls.BUFFER_LENGTH:
-                redis_client.lpop(buf_key)
+            p = pickle.loads(raw_data)
+            if p['m'] >= cls.PRECISION_THRESHOLD:
+                set_garbage(p['id'])
+            if 0 in (p['x'], p['y'], p['z']):
+                set_garbage(p['id'])
 
-            buffer = [
-                pickle.loads(item)
-                for item in redis_client.lrange(buf_key, 0, -1)
-            ]
-
-            # ???
-            # profit.
