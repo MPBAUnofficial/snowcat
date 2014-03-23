60c4acb890184e2dfdc874950aec5e59a1999b39
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-03-23 19:05:25 +0100

    added state_categorizer to categorizers list

--- categorizers/__init__.py
+++ categorizers/__init__.py
@@ -1,5 +1,11 @@
 from random_categorizer import RandomCategorizer
 from garbage_detector import GarbagePointCategorizer
+from state_categorizer import StateCategorizer
 
-all_categorizers = [RandomCategorizer, GarbagePointCategorizer]
+all_categorizers = [
+    RandomCategorizer,  # todo: remove me! :D
+    GarbagePointCategorizer,
+    StateCategorizer
+ ]
+
 __all__ = all_categorizers
