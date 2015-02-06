bfa6b08c14e76dcae612e4adfaed15e698ba700d
Marco Dalla G <marco.dallagiacoma@gmail.com>
2015-02-06 11:47:32 +0100

    lockfile added to requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "SnowCat",
-    version = "0.1",
-    packages = find_packages(exclude=('test', 'examples')),
-
-    install_requires = ['celery', 'redis', 'flask', 'msgpack-python'],
+    name="SnowCat",
+    version="0.1",
+    packages=find_packages(exclude=('test', 'examples')),
+    install_requires=[
+        'celery<3.2',
+        'redis',
+        'flask',
+        'msgpack-python',
+        'lockfile'
+    ],
     zip_safe=False,
 
-    author = "Marco Dallagiacoma",
-    author_email = "dallagiac@fbk.eu",
-    description = "A tool to setup real-time categorizers in python",
-    license = "GPL",
-    keywords = "realtime categorizer",
-    url = "https://github.com/mpbaunofficial/snowcat",
+    author="Marco Dallagiacoma",
+    author_email="dallagiac@fbk.eu",
+    description="A tool to setup real-time categorizers in python",
+    license="GPL",
+    keywords="realtime categorizer",
+    url="https://github.com/mpbaunofficial/snowcat",
 
     # could also include long_description, download_url, classifiers, etc.
 )
